c3e81c0d93e35e7b46dbb67035598299f32b75d7
ReddyAdhi <reddyadhi94@gmail.com>
2022-07-21 11:07:54 +0700

    Initial Commit

---
+++ mar_easy_swap-without-temp.js
@@ -0,0 +1,21 @@
+/**
+ * Direction:
+ * Swap these numbers without using temporary variable
+ *
+ * Expected Result:
+ * {
+ *  a: 4,
+ *  b: 2
+ * }
+ */
+let numbers = {
+  a: 2,
+  b: 4
+ };
+
+function result(numbers) {
+   // Your Code Here
+}
+ 
+console.log(result(numbers));
+ 
